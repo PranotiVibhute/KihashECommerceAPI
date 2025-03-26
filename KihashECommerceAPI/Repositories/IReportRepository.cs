a95f8a9d3d1d3bcdf298ac1d3b37d82efc819bc5
PranotiVibhute <vibhutepranoti19@gmail.com>
2025-03-26 09:22:04 +0530

    Changes

--- KihashECommerceAPI/Repositories/IReportRepository.cs
+++ KihashECommerceAPI/Repositories/IReportRepository.cs
@@ -3,7 +3,7 @@
 namespace KihashECommerceAPI.Repositories
 {
     public interface IReportRepository
-    {
+    {   
         Task<IEnumerable<CustomerOrderDto>> GetAllCustomerWithOrderDetails();
         Task<IEnumerable<CustomerOrderDto>> GetAllCustomerWithOrderDetailsLeftjoin();
         Task <IEnumerable<CustomerOrderDto?>> GetCustomerWithOrderById(int id);
